2883374648862ec72266d1a5cf1dd8bd785a640c
andrea <andrea@transformandlighting.com>
2018-12-15 18:45:51 +0100

    added max pairwise fast

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -17,6 +17,39 @@ int MaxPairwiseProduct(const std::vector<int>& numbers) {
     return max_product;
 }
 
+
+int MaxPairwiseProductFast(const std::vector<int>& numbers) {
+    
+    const auto n = numbers.size();
+    
+    size_t firstIndex =  0;
+    int    first      = -1;
+    for (size_t i = 0; i < n; ++i)
+    {
+        if(numbers[i] > first)
+        {
+            firstIndex = i;
+            first      = numbers[i];
+        }
+    }
+    
+    size_t secondIndex =  0;
+    int    second      = -1;
+    for (size_t i = 0; i < n; ++i)
+    {
+        if(numbers[i] > second && i != firstIndex)
+        {
+            secondIndex = i;
+            second      = numbers[i];
+        }
+    }
+
+     std::cout << "first "  << first  << "\n";
+     std::cout << "second " << second << "\n";
+
+    return first * second;
+}
+
 int main() {
     int n;
     std::cin >> n;
@@ -25,7 +58,7 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    int result = MaxPairwiseProduct(numbers);
+    int result = MaxPairwiseProductFast(numbers);
     std::cout << result << "\n";
     return 0;
 }
