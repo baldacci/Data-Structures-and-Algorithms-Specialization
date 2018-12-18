6f1e290ecf216952834cb8aff3e645b2ff89389b
andrea <baldacci85@hotmail.it>
2018-12-18 22:58:10 +0100

    lcm

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,20 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+size_t gcd_fast(size_t a, size_t b) {
+	if(b == 0) return a;
+	return gcd_fast(b, a % b);
+}
+
+
+size_t lcm_fast(int a, int b) {
+  return size_t(a) * (size_t(b) / gcd_fast(a,b));
+}
+
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
