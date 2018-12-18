20af32a9c09c66c91dbc1b4cd59a27dcc1d706a4
andrea <baldacci85@hotmail.it>
2018-12-18 22:14:12 +0100

    last digit fibonacci

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
@@ -16,9 +17,23 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+size_t get_fibonacci_last_digit_fibonacci_fast(int n) {
+    // write your code here
+    std::vector<size_t> fib;
+    fib.resize(n + 1, 0);
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i = 2; i < fib.size(); ++i)
+    {
+        fib[i] = (fib[i - 1] + fib[i - 2]) % 10;
+    }
+
+    return fib.back();
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fibonacci_fast(n);
     std::cout << c << '\n';
     }
