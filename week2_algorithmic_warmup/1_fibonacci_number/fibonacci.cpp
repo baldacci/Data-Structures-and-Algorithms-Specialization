20715596f6bedf2f194e91f68fc450cd4e19037a
andrea <baldacci85@hotmail.it>
2018-12-18 22:01:24 +0100

    fibonacci number

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 // The following code calls a naive algorithm for computing a Fibonacci number.
 //
@@ -21,10 +22,18 @@ int fibonacci_naive(int n) {
     return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
 }
 
-int fibonacci_fast(int n) {
+size_t fibonacci_fast(int n) {
     // write your code here
+    std::vector<size_t> fib;
+    fib.resize(n + 1, 0);
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i = 2; i < fib.size(); ++i)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
 
-    return 0;
+    return fib.back();
 }
 
 void test_solution() {
@@ -38,8 +47,8 @@ int main() {
     int n = 0;
     std::cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
+    //std::cout << fibonacci_naive(n) << '\n';
     //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
+    std::cout << fibonacci_fast(n) << '\n';
     return 0;
 }
