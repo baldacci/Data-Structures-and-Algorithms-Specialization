01dc628b9c55ad323d32ca41044d20167e387c31
andrea <baldacci85@hotmail.it>
2018-12-19 14:32:20 +0100

    knapsack

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <algorithm> 
 
 using std::vector;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
-
-  // write your code here
-
+  
+  using ItemInfo = std::pair<int, int>;
+  using SortInfo = std::pair<double, ItemInfo>;
+  
+  vector<SortInfo> items;
+  for(int i = 0; i < weights.size(); ++i)
+  {
+    const double unitValue = double(values [i]) / double(weights[i]);
+    items.emplace_back(std::make_pair(unitValue, std::make_pair(weights[i], values[i])));
+  }
+  std::sort(items.begin(), items.end(), std::greater<SortInfo>());
+  
+  for(int i = 0; i < items.size(); ++i)
+  {
+    const double uv = items[i].first;
+    const int   w  = items[i].second.first;
+    const int   x  = std::min(w, capacity);
+    capacity -= x;
+    value    += uv * double(x);
+  }
+  
   return value;
 }
 
