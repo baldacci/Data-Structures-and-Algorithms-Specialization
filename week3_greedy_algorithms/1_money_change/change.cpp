b42acdfa6a7ab724eaef800fb77d47f30e44514b
andrea <baldacci85@hotmail.it>
2018-12-19 12:34:19 +0100

    money change

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <array>
 
 int get_change(int m) {
-  //write your code here
+  std::array<int, 3> coins = {10, 5, 1};
+  int i = 0;
+  int n = 0;
+  while(m > 0)
+  {
+    while(m >= coins[i])
+    {
+        m -= coins[i];
+        ++n;
+    }
+    ++i;
+  }
   return n;
 }
 
